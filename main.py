a90dca5f8178735164b252eeb36e3296fe38e01c
zinminkhant67243246-ai <zinminkhant67243246@gmail.com>
2026-07-30 23:05:41 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_document(
+        document="Better_Sounds.mcpack",
+        caption="📦 Better Sounds ကို ဒီမှာယူပါ။"
+    )
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
